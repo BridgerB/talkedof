11113f51b13404f457a339cd988cf23963722509
BridgerB <git@bridgerb.com>
2023-03-29 16:34:16 -0600

    set default url to move to search

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+    import { goto } from "$app/navigation";
+    async function move() {
+        try {
+            await goto('/search');
+        } catch (error) {
+            console.error("Failed to navigate:", error);
+        }
+    }
+    move()
+</script>
