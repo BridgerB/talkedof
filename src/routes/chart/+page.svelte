a7f06905cb77c59f7f62ade2d291663cc770a256
BridgerB <git@bridgerb.com>
2023-04-05 14:40:12 -0600

    Adding charts

---
+++ src/routes/chart/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Chart from './Chart.svelte';
+  
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }
+      ]
+    };
+  
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    };
+  </script>
+  <section>
+    <h1>Charts</h1>
+    <Chart {data} {options} type="bar" />
+  </section>
+
+  
+
+<!-- <style lang="scss" type="text/scss">
+    @import "../../styles/app.scss";
+</style> -->
+
---
+++ src/routes/chart/Chart.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { onMount } from "svelte";
+    import Chart from "chart.js/auto";
+
+    export let data;
+    export let options;
+    export let type = "line";
+
+    let canvas;
+    let chart;
+
+    onMount(() => {
+        chart = new Chart(canvas, {
+            type,
+            data,
+            options,
+        });
+
+        return () => {
+            chart.destroy();
+        };
+    });
+</script>
+<canvas bind:this={canvas} />
