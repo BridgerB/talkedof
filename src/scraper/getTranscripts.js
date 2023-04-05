ed4d5a326044cc5d907d38e26c3628bd66852f4c
BridgerB <git@bridgerb.com>
2023-04-04 20:18:40 -0600

    refactored getTranscripts

--- src/scraper/getTranscripts.js
+++ src/scraper/getTranscripts.js
@@ -4,139 +4,153 @@ import puppeteer from 'puppeteer'; //node
 import Surreal from 'surrealdb.js'; //node
 import dotenv from 'dotenv';
 dotenv.config();
-//********************************************************************
-const channel_id = 'everyframeapainting';
-//********************************************************************
-async function get_videos() {
-    try {
-        const db = new Surreal(process.env.PRIVATE_SURREALDB_URL);
-        let tokin = await db.signin({
-            // DB: 'talkedof',
-            // SC: channel_id,
-            user: process.env.PRIVATE_USERNAME,
-            pass: process.env.PRIVATE_PASSWORD,
 
-        });
-        await db.wait();
-        await db.use(`${channel_id}`, `talkedof`);
-        console.log(`${tokin}`);
-        console.log(`Connected to namespace: ${channel_id}`);
-        let videos_to_transcribe = await db.query('SELECT * FROM videos WHERE transcribed = false AND skipped = false limit 2');
-        await db.close();
-        return await videos_to_transcribe
+const channel_name = 'lexfridman';
+const ns = 'lex'
 
-    } catch (e) {
-        console.error('ERROR', e);
-    }
-};
-async function video_loop(videos_to_transcribe) {
-    try {
-
-        for (let video of videos_to_transcribe) {
-            for (let sub_thing of video.result) {
-                console.log('Start: ************************************************************************************')
-                
-                console.log(sub_thing)
-                await get_transcript(sub_thing)
-            }
-        }
-    } catch (e) {
-        console.error('ERROR', e);
-    }
-};
-async function get_transcript(video) {
+/**
+ * Connect to the database.
+ * @returns {Promise<Surreal>} A connected Surreal instance.
+ */
+async function connectToDatabase() {
     const db = new Surreal(process.env.PRIVATE_SURREALDB_URL);
     await db.signin({
         user: process.env.PRIVATE_USERNAME,
         pass: process.env.PRIVATE_PASSWORD,
     });
-    await db.use(channel_id, `talkedof`);
-    console.log(`Connected to namespace: ${channel_id}`);
-    const browser = await puppeteer.launch({ headless: true, defaultViewport: null })
+    await db.use(ns, 'talkedof');
+    console.log(`Connected to namespace: ${ns}`);
+    return db;
+}
 
+/**
+ * Retrieve the video URLs to transcribe.
+ * @param {Surreal} db - Connected Surreal instance.
+ * @returns {Promise<Array>} List of videos to transcribe.
+ */
+async function getVideos(db) {
+    const videosToTranscribe = await db.query(
+        'SELECT * FROM videos WHERE transcribed = false AND skipped = false limit 2'
+    );
+    return videosToTranscribe;
+}
+
+/**
+ * Loop through the videos and get their transcripts.
+ * @param {Array} videosToTranscribe - List of videos to transcribe.
+ * @param {Surreal} db - Connected Surreal instance.
+ */
+async function processVideos(videosToTranscribe, db) {
+    const promises = videosToTranscribe.map(async (video) => {
+        for (const subVideo of video.result) {
+            console.log(
+                'Start: ************************************************************************************'
+            );
+            console.log(subVideo);
+            await getTranscript(subVideo, db);
+        }
+    });
+    await Promise.all(promises);
+}
+
+// Get the transcript for a video
+async function getTranscript(video, db) {
     try {
-        let url = video.url
-        let count = 0;
-        let data = []
-        let transcript = '';
-        const page = await browser.newPage()
-        await page.setViewport({ width: 1920, height: 1080 })
-        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36");
-        console.log(`Getting transcription for: ${url}`);
-        await page.goto(url)
-        // await page.setDefaultNavigationTimeout(5000);
-        await page.waitForSelector('#movie_player > .ytp-chrome-bottom > .ytp-chrome-controls > .ytp-left-controls > .ytp-play-button', { timeout: 5000 })
-        await page.click('#movie_player > .ytp-chrome-bottom > .ytp-chrome-controls > .ytp-left-controls > .ytp-play-button')
-        await page.waitForSelector('.ytd-watch-metadata > #button-shape > .yt-spec-button-shape-next > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill', { timeout: 5000 })
-        await page.click('.ytd-watch-metadata > #button-shape > .yt-spec-button-shape-next > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill')
-        await page.waitForSelector('.ytd-popup-container > #items > .style-scope > .style-scope > .style-scope:nth-child(2)', { timeout: 5000 })
-        await page.click('.ytd-popup-container > #items > .style-scope > .style-scope > .style-scope:nth-child(2)')
-        page.on('response', async (response) => {
-            const request = response.request();
-            if (request.url().includes('transcript')) {
-                console.log(`Transcript found...`);
-                let text = JSON.parse(`${await response.text()}`)
-                let transcripts = text.actions[0].updateEngagementPanelAction.content.transcriptRenderer.content.transcriptSearchPanelRenderer.body.transcriptSegmentListRenderer.initialSegments
-                // console.log(typeof transcripts)
-                if (typeof transcripts == 'object') {
-                    for (let item of transcripts) {
-                        let start = parseInt(item.transcriptSegmentRenderer.startMs);
-                        let end = parseInt(item.transcriptSegmentRenderer.endMs);
-                        if (typeof (item.transcriptSegmentRenderer.snippet.runs) === 'undefined') {
-                            console.log('Flag')
-                            transcript = ' '
-                        } else {
-                            transcript = item.transcriptSegmentRenderer.snippet.runs[0].text.trim().replace(/\n\n/g, " ");
-                        }
-                        let created = await db.create('transcripts', {
-                            url: url,
-                            start: start,
-                            end: end,
-                            transcript: transcript
-                        });
-                        console.log(created)
-                        count += 1;
-                    }
-                    console.log(`Saved ${count} lines to namespace: ${channel_id}`);
-                    try {
-                        let test = await db.query('SELECT * FROM videos limit 2');
-                        console.log(JSON.stringify(test))
-                        console.log(JSON.stringify(video))
-
-                        let updated = await db.change(video.id, {
-                            transcribed: true,
-                            lines: count,
-                        })
-
-                        console.log(updated)
-                    } catch (e) {
-                        // console.error('change error: ', e);
-                        console.error('change error...');
-                    }
-
-                    browser.close()
-                    db.close();
-                } else {
-                    console.log(`ERROR typeof transcript is not object`)
-                    browser.close()
-                    db.close();
-                }
-            }
-
-        })
+        const db = await connectToDatabase();
+        const browser = await puppeteer.launch({ headless: true, defaultViewport: null });
+        await processPage(video, db, browser);
     } catch (e) {
         console.error('ERROR', e);
         console.log('No transcript found...');
-        let updated = await db.change(video.id, {
-            skipped: true
-        })
-        console.log(updated)
-        db.close();
-        await browser.close()
+    }
+    await db.close();
+}
+
+// Process a single video page
+async function processPage(video, db, browser) {
+    const url = video.url;
+    let count = 0;
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36");
+    console.log(`Getting transcription for: ${url}`);
+    await page.goto(`${url}`);
+    await page.waitForSelector('#info > span:nth-child(3)', { timeout: 5000 });
+    await page.click('#info > span:nth-child(3)');
+    await page.waitForSelector('#movie_player > .ytp-chrome-bottom > .ytp-chrome-controls > .ytp-left-controls > .ytp-play-button', { timeout: 5000 });
+    await page.click('#movie_player > .ytp-chrome-bottom > .ytp-chrome-controls > .ytp-left-controls > .ytp-play-button');
+    await page.waitForSelector('.ytd-watch-metadata > #button-shape > .yt-spec-button-shape-next > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill', { timeout: 5000 });
+    await page.click('.ytd-watch-metadata > #button-shape > .yt-spec-button-shape-next > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill');
+    await page.waitForSelector('.ytd-popup-container > #items > .style-scope > .style-scope > .style-scope:nth-child(2)', { timeout: 5000 });
+    await page.click('.ytd-popup-container > #items > .style-scope > .style-scope > .style-scope:nth-child(2)');
+    const videoDetails = await page.evaluate(() => {
+        const jsonString = document.querySelector("#scriptTag").innerText;
+        const jsonObj = JSON.parse(jsonString);
+        return jsonObj;
+    });
+    const channel = await page.evaluate(() => {
+        const channelURL = document.querySelector("#text-container.ytd-channel-name a.yt-simple-endpoint").href;
+        const match = channelURL.match(/@(.*)/);
+        const result = match[1];
+        console.log(result);
+        return result;
+    });
+    page.on('response', async (response) => {
+        const request = response.request();
+        if (request.url().includes('transcript')) {
+            console.log(`Transcript found...`);
+            let text = JSON.parse(`${await response.text()}`);
+            let transcripts = text.actions[0].updateEngagementPanelAction.content.transcriptRenderer.content.transcriptSearchPanelRenderer.body.transcriptSegmentListRenderer.initialSegments;
+            if (typeof transcripts === 'object') {
+                await processTranscripts(transcripts, url, db, browser);
+                count = transcripts.length;
+                console.log(`Saved ${count} lines to namespace: ${ns}`);
+                await updateVideoStatus(video, db, {
+                    transcribed: true,
+                    lines: count,
+                    uploadDate: new Date(videoDetails.uploadDate),
+                    channel: channel,
+                });
+            } else {
+                console.log(`ERROR: typeof transcript is not object`);
+                console.log(transcripts)
+                await updateVideoStatus(video, db, { skipped: true });
+            }
+            await db.close();
+            await browser.close();
+        }
+    });
+}
+
+// Process the retrieved transcripts and save them to the database
+async function processTranscripts(transcripts, url, db, browser) {
+    for (let item of transcripts) {
+        let start = parseInt(item.transcriptSegmentRenderer.startMs);
+        let end = parseInt(item.transcriptSegmentRenderer.endMs);
+        let transcript = item.transcriptSegmentRenderer.snippet.runs[0]?.text?.trim()?.replace(/\n\n/g, " ") || ' ';
+        await db.create('transcripts', {
+            url: url,
+            start: start,
+            end: end,
+            transcript: transcript
+        });
     }
 }
+
+// Update the video status in the database
+async function updateVideoStatus(video, db, updateData) {
+    try {
+        await db.change(video.id, updateData);
+    } catch (e) {
+        //https://github.com/surrealdb/surrealdb.js/issues/74
+        console.error('THIS ERROR IS FROM A BUG IN SURREALDB)');
+    }
+}
+
+// Main function
 async function main() {
-    let videos_to_transcribe = await get_videos();
-    await video_loop(videos_to_transcribe)
+    const db = await connectToDatabase();
+    const videosToTranscribe = await getVideos(db);
+    await processVideos(videosToTranscribe, db);
 }
 main();
